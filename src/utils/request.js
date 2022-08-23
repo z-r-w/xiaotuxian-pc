ea759719cae631e4e5bd18af9141b5515c56782b
zrw <1763481644@qq.com>
2022-08-23 16:00:08 +0800

    request请求和响应封装

---
+++ src/utils/request.js
@@ -0,0 +1,55 @@
+import axios from 'axios'
+import store from '@/store'
+import router from '@/router'
+export const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const instance = axios.create({
+  baseURL,
+  timeout: 5000
+})
+
+instance.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  const { profile } = store.state.user
+  if (profile.token) {
+    config.headers.Authorization = `Bearer ${profile.token}`
+  }
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+instance.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response.data
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  if (error.response && error.response.state === 401) {
+    store.commit('user/setUser', {})
+    // 当前路由地址
+    // 组件里头：`/user?a=10` $route.path === /user  $route.fullPath === /user?a=10
+    // js模块中：router.currentRoute.value.fullPath 就是当前路由地址，router.currentRoute 是ref响应式数据
+    const fullPath = encodeURIComponent(router.currentRoute.value.fullPath)
+    // encodeURIComponent 转换uri编码，防止解析地址出问题
+    router.push('/login?redirectUrl' + fullPath)
+  }
+
+  return Promise.reject(error)
+})
+
+// / 请求工具函数
+export default (url, method, submitData) => {
+  // 负责发请求：请求地址，请求方式，提交的数据
+  return instance({
+    url,
+    method,
+    // 1. 如果是get请求  需要使用params来传递submitData   ?a=10&c=10
+    // 2. 如果不是get请求  需要使用data来传递submitData   请求体传参
+    // [] 设置一个动态的key, 写js表达式，js表达式的执行结果当作KEY
+    // method参数：get,Get,GET  转换成小写再来判断
+    // 在对象，['params']:submitData ===== params:submitData 这样理解
+    [method.toLowerCase() === 'get' ? 'params' : 'data']: submitData
+  })
+}
