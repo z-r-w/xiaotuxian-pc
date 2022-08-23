48cfdd9faf9dff9053db327c4a3c4d5e5d7756ff
zrw <1763481644@qq.com>
2022-08-23 23:45:57 +0800

    封装共用样式

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'normalize.css' // 重置样式包
+import '@/assets/styles/common.less' // 自己的公共样式
 createApp(App).use(store).use(router).mount('#app')
