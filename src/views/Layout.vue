f4d2ee000bad403b621dca0bc3caa6fdec617e2f
zrw <1763481644@qq.com>
2022-08-24 15:51:57 +0800

    -头部菜单布局样式

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,16 +1,21 @@
 <template>
+  <!-- 头部导航 -->
   <AppTopnav></AppTopnav>
+  <!-- 头部菜单 -->
+  <AppHearer></AppHearer>
   <header>头部</header>
   <router-view></router-view>
   <footer>底部</footer>
 </template>
 
 <script>
+import AppHearer from '@/components/app-header.vue'
 import AppTopnav from '@/components/app-topnav.vue'
 export default {
   name: 'Layout-view',
   components: {
-    AppTopnav
+    AppTopnav,
+    AppHearer
   }
 }
 </script>
