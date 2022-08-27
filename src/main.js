b37bbf9c92aee5f3f00641b8bfc35fe83317c0b6
zrw <1763481644@qq.com>
2022-08-28 06:19:44 +0800

    骨架图

--- src/main.js
+++ src/main.js
@@ -5,4 +5,6 @@ import store from './store'
 import 'normalize.css' // 重置样式包
 import '@/assets/styles/common.less' // 自己的公共样式
 import '@/assets/styles/iconfont.css' // 字体图标
-createApp(App).use(store).use(router).mount('#app')
+import ui from './components/library' // 骨架图插件
+
+createApp(App).use(store).use(router).use(ui).mount('#app')
