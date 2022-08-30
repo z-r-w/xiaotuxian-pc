73b8b173f3baab1a85a3750992bea62968673c71
zrw <1763481644@qq.com>
2022-08-30 23:03:43 +0800

    热门品牌组件懒加载及骨架图

--- src/views/home/components/home-brand.vue
+++ src/views/home/components/home-brand.vue
@@ -1,17 +1,22 @@
 <template>
-  <HomePanel title="热门品牌" sub-title="国际经典 品质保证">
+   <HomePanel title="热门品牌" sub-title="国际经典 品质保证">
     <template v-slot:right>
-       <a @click="toggle(-1)" :class="{disabled:index===0}" href="javascript:;" class="iconfont icon-angle-left prev"></a>
+      <a @click="toggle(-1)" :class="{disabled:index===0}" href="javascript:;" class="iconfont icon-angle-left prev"></a>
       <a @click="toggle(1)" :class="{disabled:index===1}" href="javascript:;" class="iconfont icon-angle-right next"></a>
     </template>
-    <div class="box" ref="box">
-       <ul v-if="brands.length" class="list" :style="{transform:`translateX(${-index*1240}px)`}">
+    <div ref="target" class="box">
+      <Transition name="fade">
+        <ul v-if="brands.length" class="list" :style="{transform:`translateX(${-index*1240}px)`}">
           <li v-for="item in brands" :key="item.id">
             <RouterLink to="/">
               <img :src="item.picture" alt="">
             </RouterLink>
           </li>
-      </ul>
+        </ul>
+        <div v-else class="skeleton">
+          <XtxSkeleton class="item" v-for="i in 5" :key="i" animated bg="#e4e4e4" width="240px" height="305px"/>
+        </div>
+      </Transition>
     </div>
   </HomePanel>
 </template>
@@ -20,16 +25,17 @@
 import HomePanel from './home-panel'
 import { ref } from 'vue'
 import { findBrand } from '@/api/home'
+import { useLazyData } from '@/hooks'
 export default {
   name: 'HomeBrand',
   components: { HomePanel },
   setup () {
   // 获取数据
-    const brands = ref([])
-    findBrand(10).then(data => {
-      brands.value = data.result
-    })
-
+    // const brands = ref([])
+    // findBrand(10).then(data => {
+    //   brands.value = data.result
+    // })
+    const { target, result } = useLazyData(() => findBrand(10))
     // 切换效果，前提只有 0 1 两页
     const index = ref(0)
     // 1. 点击上一页
@@ -39,7 +45,7 @@ export default {
       if (newIndex < 0 || newIndex > 1) return
       index.value = newIndex
     }
-    return { brands, toggle, index }
+    return { brands: result, toggle, index, target }
   }
 }
 </script>
@@ -90,4 +96,14 @@ export default {
     }
   }
 }
+ .skeleton {
+    width: 100%;
+    display: flex;
+    .item {
+      margin-right: 10px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 </style>
